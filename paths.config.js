61ed701c97dc456cf4394eda1736f48dc988833d
Kor Dwarshuis <kor@dwarshuis.com>
2020-08-03 20:01:24 +0200

    .gitignore changes.

--- paths.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  cardsContent: "https://blockchainbird.com/t/data/cards.php",
-  scoresJSON: "https://blockchainbird.com/t/data/cardgamescores.php",
-  realTimeTwitterJSON: "https://blockchainbird.com/t/twitter-phirehose/tweets-cors.php",
-  realtimeTweetsScreenMessage: "json/screen-messages.json",
-};
