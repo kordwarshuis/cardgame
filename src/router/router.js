f51178d994f2d6e4ea3c7a6f83546d075eba06d3
Kor Dwarshuis <kor@dwarshuis.com>
2020-09-04 13:50:14 +0200

    Removed unused and outcommented code.

--- src/router/router.js
+++ src/router/router.js
@@ -1,13 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import publicPath from "../../vue.config";
-
 import Home from "../views/Home.vue";
-
-// import About from "../views/About.vue";
-// import TwitterRealTime from "../views/TwitterRealTime.vue";
-// import Scores from "../views/Scores.vue";
-
 
 Vue.use(VueRouter);
 
@@ -22,28 +16,20 @@ const routes = [{
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: About
     component: () =>
-      import( /* webpackChunkName: "about" */ "../views/About.vue")
+      import("../views/About.vue")
   },
   {
     path: "/scores",
     name: "Scores",
     component: () =>
-      import( /* webpackChunkName: "about" */ "../views/Scores.vue")
+      import("../views/Scores.vue")
   },
   {
     path: "/twitter-real-time",
     name: "TwitterRealTime",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: TwitterRealTime    
     component: () =>
-      import( /* webpackChunkName: "about" */ "../views/TwitterRealTime.vue")
+      import("../views/TwitterRealTime.vue")
   }
 ];
 
@@ -52,13 +38,12 @@ const router = new VueRouter({
   // mode: 'hash', // back button not work, https://github.com/vuejs/vue-router/issues/853 ??
   mode: 'history',
   routes,
-  scrollBehavior() {//https://stackoverflow.com/a/57212309, TODO: https://router.vuejs.org/guide/advanced/scroll-behavior.html#async-scrolling
+  scrollBehavior() { //https://stackoverflow.com/a/57212309, TODO: https://router.vuejs.org/guide/advanced/scroll-behavior.html#async-scrolling
     // setTimeout(function(){document.getElementById('app').scrollIntoView();}, 3000);
     document.getElementById("app").scrollIntoView();
     document.querySelector(".modal-content").scrollIntoView();
     document.querySelector(".overlay-fullscreen .card-body").scrollIntoView();
   }
-
 });
 
 export default router;
