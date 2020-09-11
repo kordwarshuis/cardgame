a79231cfb159c534034851e00f475a885d39bede
Kor Dwarshuis <kor@dwarshuis.com>
2020-09-11 20:22:38 +0200

    Small change in wording.

--- src/components/RelatedCards.vue
+++ src/components/RelatedCards.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="p-2 mb-3 mt-5 relatedItems" v-if="this.$store.state.currentCard['Related']">
-    <h3 class="related">Related</h3>
+    <h3 class="related">Related Cards</h3>
     <p>
         <span v-for="item in this.$store.state.currentCard['Related']" v-bind:key="item" @click="$store.commit('showCardIntroFromURL', item)">
             <router-link :to="'/card/' + item">{{ item }}</router-link>
