a4f4d5b71940c1c71ac3302d15f9191c7167ee72
Kor Dwarshuis <kor@dwarshuis.com>
2020-09-15 13:36:56 +0200

    Tweet stream added at the bottom, in horizontal direction. HTML + CSS.

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,77 @@
     <Person1 />
     <Person2 />
     <!-- <Person3 /> -->
+    <div class="tweets-container">
+        <div style="" id="tweets">
+
+            <div class="tweet col-md-12">
+                <div class="card mb-4 box-shadow">
+                    <div class="card-body">
+                        <div class="card-text"><img class="img-fluid float-left w-25 mr-3" src="https://pbs.twimg.com/profile_images/1126510995763949568/TIkkcWm1_normal.jpg" alt="">
+                            <p>Name: Dirt Adventure 🔴<br>Verified: false<br>Keyword: the</p>
+                            <p>RT <a href="http://twitter.com/function(e,t){return new E.fn.init(e,t)}" title="Follow ChainPeople" target="_blank">@ChainPeople</a>: DigiByte claims to have a proven and tested solution that is faster and safer than blockchains like Bitcoin and Ethereum.…</p>
+                            <p>Followers: 1022 | <a target="_blank" rel="noopener" href="https://twitter.com/dirt_adventure/status/1305791212029239296">Go to tweet</a></p>
+                        </div>
+                        <div class="d-flex justify-content-between align-items-center"><small class="text-muted"><span class="tweetNumber">#0</span> <span class="tweetTimeStamp">🕐 10:51:12</span></small></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="tweet col-md-12">
+                <div class="card mb-4 box-shadow">
+                    <div class="card-body">
+                        <div class="card-text"><img class="img-fluid float-left w-25 mr-3" src="https://pbs.twimg.com/profile_images/1126510995763949568/TIkkcWm1_normal.jpg" alt="">
+                            <p>Name: Dirt Adventure 🔴<br>Verified: false<br>Keyword: the</p>
+                            <p>RT <a href="http://twitter.com/function(e,t){return new E.fn.init(e,t)}" title="Follow ChainPeople" target="_blank">@ChainPeople</a>: DigiByte claims to have a proven and tested solution that is faster and safer than blockchains like Bitcoin and Ethereum.…</p>
+                            <p>Followers: 1022 | <a target="_blank" rel="noopener" href="https://twitter.com/dirt_adventure/status/1305791212029239296">Go to tweet</a></p>
+                        </div>
+                        <div class="d-flex justify-content-between align-items-center"><small class="text-muted"><span class="tweetNumber">#0</span> <span class="tweetTimeStamp">🕐 10:51:12</span></small></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="tweet col-md-12">
+                <div class="card mb-4 box-shadow">
+                    <div class="card-body">
+                        <div class="card-text"><img class="img-fluid float-left w-25 mr-3" src="https://pbs.twimg.com/profile_images/1126510995763949568/TIkkcWm1_normal.jpg" alt="">
+                            <p>Name: Dirt Adventure 🔴<br>Verified: false<br>Keyword: the</p>
+                            <p>RT <a href="http://twitter.com/function(e,t){return new E.fn.init(e,t)}" title="Follow ChainPeople" target="_blank">@ChainPeople</a>: DigiByte claims to have a proven and tested solution that is faster and safer than blockchains like Bitcoin and Ethereum.…</p>
+                            <p>Followers: 1022 | <a target="_blank" rel="noopener" href="https://twitter.com/dirt_adventure/status/1305791212029239296">Go to tweet</a></p>
+                        </div>
+                        <div class="d-flex justify-content-between align-items-center"><small class="text-muted"><span class="tweetNumber">#0</span> <span class="tweetTimeStamp">🕐 10:51:12</span></small></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="tweet col-md-12">
+                <div class="card mb-4 box-shadow">
+                    <div class="card-body">
+                        <div class="card-text"><img class="img-fluid float-left w-25 mr-3" src="https://pbs.twimg.com/profile_images/1126510995763949568/TIkkcWm1_normal.jpg" alt="">
+                            <p>Name: Dirt Adventure 🔴<br>Verified: false<br>Keyword: the</p>
+                            <p>RT <a href="http://twitter.com/function(e,t){return new E.fn.init(e,t)}" title="Follow ChainPeople" target="_blank">@ChainPeople</a>: DigiByte claims to have a proven and tested solution that is faster and safer than blockchains like Bitcoin and Ethereum.…</p>
+                            <p>Followers: 1022 | <a target="_blank" rel="noopener" href="https://twitter.com/dirt_adventure/status/1305791212029239296">Go to tweet</a></p>
+                        </div>
+                        <div class="d-flex justify-content-between align-items-center"><small class="text-muted"><span class="tweetNumber">#0</span> <span class="tweetTimeStamp">🕐 10:51:12</span></small></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="tweet col-md-12">
+                <div class="card mb-4 box-shadow">
+                    <div class="card-body">
+                        <div class="card-text"><img class="img-fluid float-left w-25 mr-3" src="https://pbs.twimg.com/profile_images/1126510995763949568/TIkkcWm1_normal.jpg" alt="">
+                            <p>Name: Dirt Adventure 🔴<br>Verified: false<br>Keyword: the</p>
+                            <p>RT <a href="http://twitter.com/function(e,t){return new E.fn.init(e,t)}" title="Follow ChainPeople" target="_blank">@ChainPeople</a>: DigiByte claims to have a proven and tested solution that is faster and safer than blockchains like Bitcoin and Ethereum.…</p>
+                            <p>Followers: 1022 | <a target="_blank" rel="noopener" href="https://twitter.com/dirt_adventure/status/1305791212029239296">Go to tweet</a></p>
+                        </div>
+                        <div class="d-flex justify-content-between align-items-center"><small class="text-muted"><span class="tweetNumber">#0</span> <span class="tweetTimeStamp">🕐 10:51:12</span></small></div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
     <Toasts :time-out="2000"></Toasts>
 </div>
 </template>
@@ -47,8 +118,8 @@ export default {
         // this.soundSetting();
         this.initSound();
         realTimeTweets.start({
-    source: process.env.VUE_APP_REALTIME_TWITTER_JSON
-});
+            source: process.env.VUE_APP_REALTIME_TWITTER_JSON
+        });
 
     },
     methods: {
@@ -161,6 +232,44 @@ export default {
 
 <style lang="scss">
 @import "~bootstrap/dist/css/bootstrap.min.css";
+
+.tweets-container {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    height: 100px;
+    // max-height: 100px;
+    width: 100%;
+    margin: 0;
+    // border: 3px solid red;
+    overflow: scroll;
+
+    // z-index: 10000000000000000;
+
+    font-size: 0.6em;
+    color: #000;
+    //  max-height: 3em;
+    transition: all 0.3s ease-in-out;
+}
+
+.tweets-container:hover {
+    // transform: translate(0px, -200px);
+    height: 300px;
+    transition: all 0.3s ease-in-out;
+    // position: absolute;
+}
+
+#tweets {
+    width: 100000000000000000000px;
+}
+
+.tweet {
+    max-width: 300px;
+    min-height: 400px;
+    float: left;
+    display: block;
+    // transition: opacity 0.3s ease-in-out;
+}
 
 // @font-face {
 //     font-family: 'League Gothic';
