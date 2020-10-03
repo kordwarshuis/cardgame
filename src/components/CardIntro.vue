ead8ce36c3d7a4e1589ecf1c115810c5cfb8d2d2
Kor Dwarshuis <kor@dwarshuis.com>
2020-10-03 16:22:53 +0200

    CSS removed unused code, working towards design by JV Creative.

--- src/components/CardIntro.vue
+++ src/components/CardIntro.vue
@@ -1,53 +1,46 @@
 <template>
 <!-- open/close -->
-<div class="overlay-fullscreen overlay-fullscreen-contentpush" :class="this.$store.state.cssClassCardIntroState">
-    <div class="row">
-
-        <div class="col-lg-12 col-sm-12 ">
-            <div class="social-media-container">
-                <SocialMedia />
-            </div>
-            <button class="closeCardIntro" title="All cards">All cards</button>
-            <button class="copyURLtoClipboard copyURLtoClipboard3 " title="Copy Link">Copy Link</button>
-        </div>
-
-        <div class="col-lg-6 col-sm-6 column2 p-0 pb-5" style="border-right: 1px solid #ddd;">
+<div class="overlay-fullscreen overlay-fullscreen-contentpush p-0" :class="this.$store.state.cssClassCardIntroState">
+    <div class="row m-0 pb-5">
+        <div class="col-lg-6 col-sm-6">
             <div class="card h-100 bg-transparent" style="border:none;">
-                <div class="card-body">
+                <div class="card-body" style="border-right: 1px solid #666666;">
                     <h3 class="pt-5">Misconception:</h3>
                     <div class="p-3 mt-2 text-center" style="background-color: #2F3658; min-height: 13em; border-radius: 10px;">
-                        <p>{{ this.$store.state.currentCard["Prejudice"] }} …</p>
-                        <button class="btn btn-outline-light mt-3" style="cursor:pointer;" @click="showCardFull">Open full card</button>
+                        <p class="typed mb-5">{{ getPrejudice }} …</p>
+                        <button class="btn btn-outline-light mt-5 d-block ml-auto mr-auto" style="cursor:pointer;" @click="showCardFull">Open full card</button>
                     </div>
-
+                    <RelatedCards />
                 </div>
             </div>
         </div>
-        <div class="col-lg-6 col-sm-6 column1 p-0 ">
+        <div class="col-lg-6 col-sm-6 ">
             <div class="card h-100 bg-transparent" style="border:none;">
-                <div class="card-body title-on-card center">
+                <div class="card-body title-on-card center" style="">
                     <h3 class="pt-5" style="color: #5FE2FC;">Reply:</h3>
-                    <h2 class="title-on-card-text animated infinite">
+                    <h2 class="title-on-card-text">
                         <span class="quote">“</span>
-                        <span class="text">{{ getPrejudice }}</span>
+                        <span class="text" style="font-size: 0.6em;">{{ this.$store.state.currentCard["short direct answer"] }}</span>
                         <span class="quote">”</span>
                     </h2>
+                    <hr>
+                    <div v-if="(this.$store.state.currentCard['Youtube Video Id'])" class="col-lg-12 col-sm-12 p-0" style="font-size: 0.9em;">
+                        <VideoBare />
+                    </div>
+                    <!-- <div v-if="(this.$store.state.currentCard['Quiz'])" class="col-lg-12 col-sm-12">
+                        <Quiz />
+                    </div> -->
 
                 </div>
             </div>
         </div>
 
-        <div class="col-lg-12 col-sm-12">
-            <RelatedCards />
+        <div class="col-lg-12 col-sm-12 m-0" style="position: fixed; bottom: 0; left: 0;background: linear-gradient(to right, #5C34A7, #2376D6);">
+            <div style="display: inline-block;">
+                <SocialMedia />
+            </div>
+            <button class="copyURLtoClipboard copyURLtoClipboard3 " style="display: inline-block;" title="Copy Link">Copy Link</button>
         </div>
-
-        <div v-if="(this.$store.state.currentCard['Youtube Video Id'])" class="col-lg-12 col-sm-12 mt-5">
-            <Video />
-        </div>
-        <div v-if="(this.$store.state.currentCard['Quiz'])" class="col-lg-12 col-sm-12">
-            <Quiz />
-        </div>
-
         <a class="overlay-fullscreen-close">×</a>
     </div>
 </div>
@@ -57,32 +50,32 @@
 import SocialMedia from "@/components/SocialMedia.vue";
 import RelatedCards from "@/components/RelatedCards.vue";
 import Quiz from "@/components/Quiz.vue";
-import Video from "@/components/Video.vue";
-
+import VideoBare from "@/components/VideoBare.vue";
 // import Quiz from "@/components/Quiz.vue";
+
 export default {
     name: "CardIntro",
+    components: {
+        SocialMedia,
+        RelatedCards,
+        VideoBare
+        // Quiz
+    },
     data() {
         return {}
     },
     computed: {
         getPrejudice: function () {
-            return this.$store.state.currentCard["short direct answer"];
+            return this.$store.state.currentCard["Prejudice"];
         }
     },
     watch: {
         getPrejudice(newValue, oldValue) {
-            this.typeWriter(".title-on-card-text .text", newValue, 20);
+            this.typeWriter(".typed", newValue, 20);
         }
     },
     mounted: function () {
         this.handleCardIntro();
-    },
-    components: {
-        SocialMedia,
-        RelatedCards,
-        Quiz,
-        Video
     },
     methods: {
         typeWriter(selector, prejudice, interval) {
@@ -200,23 +193,23 @@ export default {
     width: 100%;
     max-width: 70em;
     height: 100%;
-    // top: 0;
-    // left: 0;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     box-shadow: 0px 0px 37px 0px rgba(0, 0, 0, 0.75);
     color: $overlayItemText;
-    // background: $overlayItemBackground;
     background: #474E71;
-    padding: 0em;
+    border-radius: 10px;
+    outline: 2000px solid rgba(34, 34, 34, 0.8);
 }
 
 /* Small devices (landscape phones, 576px and up) */
-@media (min-width: 576px) {
+@media (min-width: 576px) {}
+
+/* Medium devices (tablets, 768px and up) The navbar toggle appears at this breakpoint */
+@media (min-width: 768px) {
     #app .overlay-fullscreen {
-        padding-left: 2em;
-        padding-right: 2em;
+        height: 70%;
     }
 }
 
@@ -232,7 +225,6 @@ export default {
     right: 20px;
     border: none;
     background: $_background1BCB;
-    padding: 0.1em;
     border: 1px solid transparent;
     border-radius: 50%;
     padding: 0.2em;
@@ -240,59 +232,6 @@ export default {
     font-size: 2.5em;
     line-height: 0.6;
     cursor: pointer;
-}
-
-// read more / plus sign on the intro card
-a.md-trigger {
-    border: 3px solid $menuItemsAndButtonsBorder;
-    background: $background;
-    color: $linksText;
-    border-radius: 50%;
-    padding: 0.15em 0.2em 0.25em;
-    margin: 0;
-    line-height: 0.58;
-    font-size: 1.5em;
-    vertical-align: middle;
-    display: inline-block;
-    padding: 0.5em;
-    margin: 0.3em;
-    margin-left: 0;
-    transition: background 0.3s ease-in-out;
-    cursor: pointer;
-}
-
-.md-trigger:hover {
-    background: $linksHoverBackground;
-    transition: background 0.3s ease-in-out;
-}
-
-.md-trigger {
-    transform: translateZ(0);
-    animation: background-pulsate 24s ease-out 0s 10;
-}
-
-// read more button:
-@keyframes background-pulsate {
-    0% {
-        /*letter-spacing: 1px;*/
-        background-color: $background;
-        border-color: #c70606;
-        color: #c70606;
-    }
-
-    50% {
-        /*letter-spacing: 3px;*/
-        background-color: $cardBackground;
-        border-color: $menuItemsAndButtonsBorder;
-        color: $linksText;
-    }
-
-    100% {
-        /*letter-spacing: 1px;*/
-        background-color: $background;
-        border-color: #c70606;
-        color: #c70606;
-    }
 }
 
 .container.overlay-fullscreen-open::after {
@@ -321,33 +260,11 @@ a.md-trigger {
 
 /* Small devices (landscape phones, 576px and up) */
 @media (min-width: 576px) {
-
     .title-on-card h2 {
         // >… this is a two column lay out
         font-size: 4vw;
     }
 }
-
-.closeCardIntro {
-    background: transparent url(../assets/img/icons/flat/grid.svg) no-repeat center;
-    background-size: contain;
-    // padding: 3em 1.2em 3em 2em;
-    padding: 0.7em;
-    margin-bottom: 1em;
-    z-index: auto;
-    transition: transform 0.3s;
-    color: transparent;
-}
-
-.closeCardIntro:hover,
-.copyURLtoClipboard:hover {
-    transform: rotate(360deg);
-    transition: transform 0.3s;
-}
-
-// .title-on-card {
-//     padding: 0;
-// }
 
 .title-on-card h2 {
     // padding: 1em;
@@ -355,13 +272,6 @@ a.md-trigger {
     font-size: 2rem;
 
 }
-
-// .title-on-card h2 {
-// border: 2px solid $cardBorder;
-// background: $cardBackground;
-// }
-
-$column2Border: #2f4fe036;
 
 .social-media-container {
     display: inline-block;
