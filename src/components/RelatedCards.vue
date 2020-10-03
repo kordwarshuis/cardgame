c1e95f78584c03239906cc4c21fdff2b0ecd7dd4
Kor Dwarshuis <kor@dwarshuis.com>
2020-10-03 13:41:38 +0200

    CSS changed some styls.

--- src/components/RelatedCards.vue
+++ src/components/RelatedCards.vue
@@ -1,9 +1,9 @@
 <template>
-<div class="p-2 mb-3 text-center relatedItems" v-if="this.$store.state.currentCard['Related']">
-    <h3 class="related">Related Cards</h3>
+<div class="p-2 mb-3 mt-3 text-center relatedItems" v-if="this.$store.state.currentCard['Related']">
+    <h3 class="related">Related</h3>
     <p>
         <span v-for="item in this.$store.state.currentCard['Related']" v-bind:key="item" @click="$store.commit('showCardIntroFromURL', item)">
-            <router-link :to="'/card/' + item">{{ item }}</router-link>
+            <router-link style="color: #eee;" :to="'/card/' + item">{{ item }}</router-link>
         </span>
     </p>
 </div>
