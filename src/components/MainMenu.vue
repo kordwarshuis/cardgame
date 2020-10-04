d1387f9fa978c5f1bd2bff9ea5d6057a4d4c9c7d
Kor Dwarshuis <kor@dwarshuis.com>
2020-10-04 22:06:23 +0200

    CSS main menu horizontal scrollable.

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,21 +1,25 @@
 <template>
-<ul class="nav mb-3 pt-2">
-    <li class="nav-item">
-        <router-link class="nav-link" to="/">Cards</router-link>
-    </li>
-    <li class="nav-item">
-        <router-link class="nav-link" to="/about">About</router-link>
-    </li>
-    <li class="nav-item" v-if="this.$store.state.gameName === 'Bitcoin'">
-        <router-link class="nav-link" v-if="this.$store.state.gameName === 'Bitcoin'" to="/twitter-real-time">Realtime</router-link>
-    </li>
-    <li style="border-right: none;" class="nav-item" v-if="this.$store.state.gameName === 'Bitcoin'">
-        <router-link class="nav-link" v-if="this.$store.state.gameName === 'Bitcoin'" to="/scores">Scores</router-link>
-    </li>
-    <li style="border-right: none;">
-        <SoundToggle />
-    </li>
-</ul>
+<div class="navbar-nav-scroll">
+    <!-- <ul class="nav mb-3 pt-2"> -->
+    <ul class="navbar-nav bd-navbar-nav flex-row">
+        <li class="nav-item">
+            <router-link class="nav-link" to="/">Cards</router-link>
+        </li>
+        <li class="nav-item">
+            <router-link class="nav-link" to="/about">About</router-link>
+        </li>
+        <li class="nav-item" v-if="this.$store.state.gameName === 'Bitcoin'">
+            <router-link class="nav-link" v-if="this.$store.state.gameName === 'Bitcoin'" to="/twitter-real-time">Realtime</router-link>
+        </li>
+        <li style="border-right: none;" class="nav-item" v-if="this.$store.state.gameName === 'Bitcoin'">
+            <router-link class="nav-link" v-if="this.$store.state.gameName === 'Bitcoin'" to="/scores">Scores</router-link>
+        </li>
+
+        <li style="border-right: none;">
+            <SoundToggle />
+        </li>
+    </ul>
+</div>
 </template>
 
 <script>
@@ -32,22 +36,37 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 
 <style lang="scss" scoped>
-// #nav a {
-//     display: inline-block;
-//     padding: 1em;
-//     color: #eee;
-// }
+// scrollable menu, more or less copied from https://getbootstrap.com/docs/4.5/assets/css/docs.min.css
 
-.nav {
+.navbar-nav-scroll {
     color: #ddd;
     background: #191F3A;
+    padding-right: 2em;
+
+    margin-right: -1em;
+    margin-bottom: 1em;
+    margin-left: -1em;
 }
 
-.nav li {
-    border-right: 1px solid #666;
+.navbar-nav-scroll li {
+    border-right: 1px solid #444;
+    padding: 0.5em 1em;
 }
 
-.nav a {
+.navbar-nav-scroll a {
     color: #ddd;
+}
+
+@media (max-width: 992px) {
+    .navbar-nav-scroll {
+        max-width: calc(100%+2em);
+        overflow: hidden;
+    }
+
+    .navbar-nav-scroll .navbar-nav {
+        overflow-x: auto;
+        white-space: nowrap;
+        -webkit-overflow-scrolling: touch
+    }
 }
 </style>
