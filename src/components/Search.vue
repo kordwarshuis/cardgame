bd6766b4efb949bb59cb20736b382f6ba5082ae5
Kor Dwarshuis <kor@dwarshuis.com>
2020-10-04 13:45:25 +0200

    Styled search input according to JC Creative design.

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,18 +1,15 @@
 <template>
 <div class="">
-    <div class=" pt-8 m-1 mt-2 text-center">
+    <div class=" pt-8 m-1 mt-2 mb-4 text-center">
         <VueFuse placeholder="Search" event-name="results" :list="this.$store.state.theJSON" :keys="['Prejudice', 'Prejudice Elaborate']" :defaultAll="false" class="border " />
 
     </div>
 
     <div v-for="book in results" :key="book.Prejudice" @click="$store.commit('showCardIntroFromURL', book['Unique URL'])">
         <router-link :to="'/card/' + book['Unique URL']">
-        <h3 style="cursor: pointer" class="w-1/4">{{ book.Prejudice }}</h3>
-        <p style="cursor: pointer" class="ml-4 w-3/4">{{ book['Prejudice Elaborate'] }}</p>
+            <h3 style="cursor: pointer" class="w-1/4">{{ book.Prejudice }}</h3>
+            <p style="cursor: pointer" class="ml-4 w-3/4">{{ book['Prejudice Elaborate'] }}</p>
         </router-link>
-
-
-
 
     </div>
 </div>
@@ -45,4 +42,11 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 
 <style scoped lang="scss">
+
+input {
+    background: #393D4E;
+    border-radius: 7px;
+}
+
+
 </style>
