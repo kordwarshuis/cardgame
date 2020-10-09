e24a10c11c9b8df08c15feab0a14baefc98b497e
Kor Dwarshuis <kor@dwarshuis.com>
2020-10-09 14:36:47 +0200

    Import and use animate.css.

--- src/main.js
+++ src/main.js
@@ -19,10 +19,13 @@ import _ from "lodash";
 //https://github.com/rakk7/vue-bootstrap-toasts
 import VueBootstrapToasts from "vue-bootstrap-toasts";
 
+import animated from 'animate.css';
+
 Vue.use(d3);
 Vue.config.productionTip = false;
 Vue.use(VueBootstrapToasts);
 Vue.use(Howl, Howler);
+Vue.use(animated);
 // Vue.use(interact);
 
 
