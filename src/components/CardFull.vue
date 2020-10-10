8443e050b6979a1bafe33a4e433226486a2d51a5
Kor Dwarshuis <kor@dwarshuis.com>
2020-10-10 17:05:58 +0200

    Scss to scoped.

--- src/components/CardFull.vue
+++ src/components/CardFull.vue
@@ -254,7 +254,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 
-<style lang="scss">
+<style lang="scss" scoped>
 /*! https://tympanus.net/codrops/2013/06/25/nifty-modal-window-effects/ */
 
 // MODAL
