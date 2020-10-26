ad7bf31d97656a73055b2aa6a1af589bc55a9b8b
Kor Dwarshuis <kor@dwarshuis.com>
2020-10-26 18:32:32 +0100

    Better handling of configurable search strings

--- src/components/TwitterRealTimeConfigModal.vue
+++ src/components/TwitterRealTimeConfigModal.vue
@@ -16,7 +16,7 @@
                             <div class="alert alert-info mt-3 col-md-12" role="alert">
                                 All changes work immediately.
                             </div>
-
+                            <hr class="mt-5">
                             <h2 class="mt-3">Followers</h2>
                             <label class="" for="followers" id="labelFollowers">Poster has </label>
                             <select name="followers" id="followers">
@@ -31,22 +31,27 @@
                                 <option value="1000000">1 million</option>
                             </select>
                             or more followers.
+                            <hr class="mt-5">
                             <h2 class="mt-3">Verified</h2>
                             <input class="mr-3" type="checkbox" id="onlyVerifiedAccounts" name="onlyVerifiedAccounts">
                             <label for="onlyVerifiedAccounts">Show only verified accounts.</label>
-
+                            <hr class="mt-5">
                             <div class="form-group">
-                                <h2 class="mt-3">Any of these words</h2>
-                                <label class="" for="anyOfTheseWordsUsersChoice" id="labelAnyOfTheseWordsUsersChoice">Any of these words</label>
-                                <textarea id="anyOfTheseWordsUsersChoice" name="anyOfTheseWordsUsersChoice" rows="5" class="form-control block p-3"></textarea>
-                                <!-- <button id="restoreKeyWords" type="button" class="btn btn-light mr-2">Reset</button><button id="emptyKeyWords" type="button" class="btn btn-light">Empty</button> -->
-                            </div>
+                                <h2 class="mt-3">Any of these strings of words</h2>
+                                <div class="alert alert-info mt-3 col-md-12" role="alert">
+                                    All changes work immediately.
+                                </div>
 
+                                <label class="" for="anyOfTheseStrings" id="labelAnyOfTheseStrings">Any of these strings of words, separated by a comma. Add your own:</label>
+                                <textarea id="anyOfTheseStrings" name="anyOfTheseStrings" rows="5" class="form-control block p-3">bitcoin will never, bitcoin can never, bitcoin just is not, bitcoin is one big, criminals, slow, laundering, energy, complicated, unfair, quantum, tax evaders, unsustainable, intrinsic value, shut down, scammers, roulette, only 21, not safe, black market, terrorists, tulip, greater fool, not scalable, anarchists, distribution unfair, hacked, anonymous, unsustainable, useless, ponzi, no backing, will die, forbidden, shut down, scammers, not gdpr, price down, terrorists, privacy breach, volatile, useless, deflation, chinese</textarea>
+                                <button id="restoreAnyOfTheseStringsDefault" type="button" class="btn btn-light mr-2">Reset</button><button id="emptyAnyOfTheseStrings" type="button" class="btn btn-light">Empty</button>
+                            </div>
+                            <hr class="mt-5">
                             <div class="form-group">
                                 <h2 class="mt-3">None of these words</h2>
                                 <label class="" for="noneOfTheseWordsUsersChoice" id="labelNoneOfTheseWordsUsersChoice">None of these words</label>
                                 <textarea id="noneOfTheseWordsUsersChoice" name="noneOfTheseWordsUsersChoice" rows="5" class="form-control block p-3"></textarea>
-                                <!-- <button id="restoreKeyWords" type="button" class="btn btn-light mr-2">Reset</button><button id="emptyKeyWords" type="button" class="btn btn-light">Empty</button> -->
+                                <!-- <button id="restoreAnyOfTheseStringsDefault" type="button" class="btn btn-light mr-2">Reset</button><button id="emptyAnyOfTheseStrings" type="button" class="btn btn-light">Empty</button> -->
                             </div>
 
                             <p>Separate the words with a comma. Spaces matter, “action” will also find “traction”, “ action” wont.</p>
@@ -93,7 +98,7 @@ export default {
     name: "TwitterRealTimeConfigModal",
     mounted: function () {
         this.setFollowersNumber();
-        this.setAnyOfTheseWordsUsersChoice();
+        this.handleAnyOfTheseStrings();
         this.setOnlyVerifiedAccountsUsersChoice();
     },
     methods: {
@@ -106,24 +111,56 @@ export default {
             }, false);
 
         },
-        setAnyOfTheseWordsUsersChoice() {
-            var anyOfTheseWordsUsersChoice = document.querySelector("#anyOfTheseWordsUsersChoice");
 
-            anyOfTheseWordsUsersChoice.addEventListener("input", function () {
-                var keyWordsInTextarea = this.value.split(",");
-                console.log('keyWordsInTextarea: ', keyWordsInTextarea);
+        handleAnyOfTheseStrings() {
+            var domAnyOfTheseStrings = document.querySelector("#anyOfTheseStrings");
+            var domRestoreAnyOfTheseStringsDefault = document.querySelector("#restoreAnyOfTheseStringsDefault");
+            var domEmptyAnyOfTheseStrings = document.querySelector("#emptyAnyOfTheseStrings");
 
-                realTimeTweets.setAnyOfTheseWordsUsersChoice(keyWordsInTextarea);
+            function storeDefaultKeywordsInLocalStorage(stringsInTextarea) {
+                localStorage.setItem("defaultSearchStrings", stringsInTextarea);
+            }
+
+            function emptyAnyOfTheseStrings() {
+                domAnyOfTheseStrings.value = "";
+            }
+
+            // get default string from array
+            function restoreAnyOfTheseStringsDefault() {
+                domAnyOfTheseStrings.value = realTimeTweets.anyOfTheseStringsDefault.toString();
+            }
+
+            // first save default search strings to a backup array
+            realTimeTweets.anyOfTheseStringsDefault = domAnyOfTheseStrings.value.split(",");
+            // then save it to the working array 
+            realTimeTweets.setAnyOfTheseStrings(domAnyOfTheseStrings.value.split(","));
+
+            if (localStorage.getItem("defaultSearchStrings") !== null) {
+                // then replace default string with what is in local storage, if there is any
+                domAnyOfTheseStrings.value = localStorage.getItem("defaultSearchStrings");
+                // and pass it to the working array
+                console.log('and pass it to the working array: ');
+                realTimeTweets.setAnyOfTheseStrings(localStorage.getItem("defaultSearchStrings").split(","));
+            }
+
+            domAnyOfTheseStrings.addEventListener("input", function () {
+                var stringsInTextarea = this.value.split(",");
+                // save default search strings to array
+                realTimeTweets.setAnyOfTheseStrings(stringsInTextarea);
+                storeDefaultKeywordsInLocalStorage(stringsInTextarea);
             }, false);
+
+            domRestoreAnyOfTheseStringsDefault.addEventListener("click", restoreAnyOfTheseStringsDefault, false);
+            domEmptyAnyOfTheseStrings.addEventListener("click", emptyAnyOfTheseStrings, false);
         },
         setNoneOfTheseWordsUsersChoice() {
             var noneOfTheseWordsUsersChoice = document.querySelector("#noneOfTheseWordsUsersChoice");
 
             noneOfTheseWordsUsersChoice.addEventListener("input", function () {
-                var keyWordsInTextarea = this.value.split(",");
-                console.log('keyWordsInTextarea: ', keyWordsInTextarea);
+                var stringsInTextarea = this.value.split(",");
+                console.log('stringsInTextarea: ', stringsInTextarea);
 
-                realTimeTweets.setNoneOfTheseWordsUsersChoice(keyWordsInTextarea);
+                realTimeTweets.setNoneOfTheseWordsUsersChoice(stringsInTextarea);
             }, false);
         },
         setOnlyVerifiedAccountsUsersChoice() {
@@ -143,7 +180,7 @@ export default {
 
                 realTimeTweets.setOnlyVerifiedAccountsUsersChoice(verified);
             }, false);
-        }
+        },
 
     }
 };
