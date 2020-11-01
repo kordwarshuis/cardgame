5f91d7674b09511adb964e7bd02adcc1ffe12a26
Kor Dwarshuis <kor@dwarshuis.com>
2020-11-01 19:47:04 +0100

    Changed svg background image to png

--- src/components/twitterRealtime3.vue
+++ src/components/twitterRealtime3.vue
@@ -596,14 +596,14 @@ https://tympanus.net/codrops/2014/09/16/off-canvas-menu-effects/
 }
 
 .tweets-selected-open-close-button {
-    background: url("../assets/img/icons/jv-creative/tweets-selected-open.svg") no-repeat center;
+    background: url("../assets/img/icons/jv-creative/tweets-selected-open.png") no-repeat center;
     background-size: cover;
     width: 30px;
     height: 30px;
 }
 
 .open .tweets-selected-open-close-button {
-    background: url("../assets/img/icons/jv-creative/tweets-selected-close.svg") no-repeat center !important;
+    background: url("../assets/img/icons/jv-creative/tweets-selected-close.png") no-repeat center !important;
     background-size: cover;
     width: 30px;
     height: 30px;
