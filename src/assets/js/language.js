da74e9432f2d914f99f6fc88a9607772460ab285
Kor Dwarshuis <kor@dwarshuis.com>
2020-11-07 20:40:57 +0100

    Added text strings

--- src/assets/js/language.js
+++ src/assets/js/language.js
@@ -1,6 +1,8 @@
 export var language = {
     tweetStream: {
-        instruction1: "Reply to this tweet"
+        message1: "<div class='tweet-stream-messages'><ul><li><h2>Find a tweet</h2></li><li><h2>Find a card</h2></li><li><h2>Reply with card</h2></li></ul></div>",
+        message2: "<div class='tweet-stream-messages'><h2>Gain eternal fame, spread bitcoin expertise</h2></div>",
+        message3: "<div class='tweet-stream-messages'><h2>Step forward, spread bitcoin expertise</h2></div>"
     }
 
 };
