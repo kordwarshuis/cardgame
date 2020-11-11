d8e3fe5df8495290a1837a0a0cb862b652808ea3
Kor Dwarshuis <kor@dwarshuis.com>
2020-11-11 22:48:44 +0100

    Template for quiz question

---
+++ src/components/MaxiQuizQuestions/QTemplate.vue
@@ -0,0 +1,41 @@
+<template>
+        <section id='v1flsnyhkbjwetpojn' class='snelleovergang clearfix'>
+        <div class='tekst clearfix p-3'>
+            <div class='w30em displayinlineblock'>
+                <hr>
+
+                <h2 class="nummering">–</h2>
+                <h3>xxx</h3>
+                <form class='maxiquiz'>
+                    <fieldset>
+                        <legend>Choose the right answer:</legend>
+                        <label>XXX
+                            <input type="radio" class="maxiQuizVraag" name="maxiQuizVraag" value="goud" /></label>
+                        <label>XXX
+                            <input type="radio" class="maxiQuizVraag" name="maxiQuizVraag" /></label>
+                        <label>XXX
+                            <input type="radio" class="maxiQuizVraag" name="maxiQuizVraag" /></label>
+                        <label>XXX
+                            <input type="radio" class="maxiQuizVraag" name="maxiQuizVraag" /></label>
+                        <label>XXX
+                            <input type="radio" class="maxiQuizVraag" name="maxiQuizVraag" /></label>
+                        <label>XXX
+                            <input type="radio" class="maxiQuizVraag" name="maxiQuizVraag" /></label>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+        <div class="tekst clearfix toelichting displayNone almost-visible" style="">
+            <p>xxx</p>
+            <p class="volgende-pijl">↓</p>
+            <p class="volgende-tekst">scroll down</p>
+            <p class="score"></p>
+        </div>
+    </section>
+</template>
+<script>
+
+export default {
+    name: "QTemplate"
+}
+</script>
