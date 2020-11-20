66776598116a47eb8c9e1c566a75d91be3736cdb
Kor Dwarshuis <kor@dwarshuis.com>
2020-11-20 20:28:11 +0100

    Remove html code

--- src/assets/js/language1.js
+++ src/assets/js/language1.js
@@ -1,17 +1,17 @@
 export var language = {
     tweetStream: {
-        message1: "<div class='tweet-stream-messages'><button class='close'><span class='visuallyhidden'>remove this tweet</span>×</button><ul><li><h2>Find a tweet</h2></li><li><h2>Find a card</h2></li><li><h2>Reply with card</h2></li></ul></div>",
-        message1a: "<div class='tweet-stream-messages'><button class='close'><span class='visuallyhidden'>remove this tweet</span>×</button><ul><li><h2>Find a tweet</h2></li><li><h2>Find a card</h2></li><li><h2>Reply with card</h2></li></ul></div>",
-        message1b: "<div class='tweet-stream-messages'><button class='close'><span class='visuallyhidden'>remove this tweet</span>×</button><ul><li><h2>Find a tweet</h2></li><li><h2>Find a card</h2></li><li><h2>Reply with card</h2></li></ul></div>",
-        message1c: "<div class='tweet-stream-messages'><button class='close'><span class='visuallyhidden'>remove this tweet</span>×</button><ul><li><h2>Find a tweet</h2></li><li><h2>Find a card</h2></li><li><h2>Reply with card</h2></li></ul></div>",
-        message1d: "<div class='tweet-stream-messages'><button class='close'><span class='visuallyhidden'>remove this tweet</span>×</button><ul><li><h2>Find a tweet</h2></li><li><h2>Find a card</h2></li><li><h2>Reply with card</h2></li></ul></div>",
-        message1e: "<div class='tweet-stream-messages'><button class='close'><span class='visuallyhidden'>remove this tweet</span>×</button><ul><li><h2>Find a tweet</h2></li><li><h2>Find a card</h2></li><li><h2>Reply with card</h2></li></ul></div>",
-        message2: "<div class='tweet-stream-messages'><button class='close'><span class='visuallyhidden'>remove this tweet</span>×</button><h2>Gain eternal fame, spread bitcoin expertise</h2></div>",
-        message3: "<div class='tweet-stream-messages'><button class='close'><span class='visuallyhidden'>remove this tweet</span>×</button><h2>Step forward, spread bitcoin expertise</h2></div>",
-        message4: "<div class='tweet-stream-messages'><button class='close'><span class='visuallyhidden'>remove this tweet</span>×</button><h2>Get recognition, spread bitcoin expertise</h2></div>",
-        message5: "<div class='tweet-stream-messages'><button class='close'><span class='visuallyhidden'>remove this tweet</span>×</button><p>Play withthe configuration options to change the keywords, number of followers etc.</p></div>",
-        message6: "<div class='tweet-stream-messages'><button class='close'><span class='visuallyhidden'>remove this tweet</span>×</button><p>Depending on your search configurations it can take a while before real time tweets show.</p></div>"
-        // message7: "<div class='tweet-stream-messages'><button class='close'><span class='visuallyhidden'>remove this tweet</span>×</button><p>Play withthe configuration options to change the keywords, number of followers etc.</p></div>"
+        message1: "<ul><li><h2>Find a tweet</h2></li><li><h2>Find a card</h2></li><li><h2>Reply with card</h2></li></ul>",
+        message1a: "<ul><li><h2>Find a tweet</h2></li><li><h2>Find a card</h2></li><li><h2>Reply with card</h2></li></ul>",
+        message1b: "<ul><li><h2>Find a tweet</h2></li><li><h2>Find a card</h2></li><li><h2>Reply with card</h2></li></ul>",
+        message1c: "<ul><li><h2>Find a tweet</h2></li><li><h2>Find a card</h2></li><li><h2>Reply with card</h2></li></ul>",
+        message1d: "<ul><li><h2>Find a tweet</h2></li><li><h2>Find a card</h2></li><li><h2>Reply with card</h2></li></ul>",
+        message1e: "<ul><li><h2>Find a tweet</h2></li><li><h2>Find a card</h2></li><li><h2>Reply with card</h2></li></ul>",
+        message2: "<h2>Gain eternal fame, spread bitcoin expertise</h2>",
+        message3: "<h2>Step forward, spread bitcoin expertise</h2>",
+        message4: "<h2>Get recognition, spread bitcoin expertise</h2>",
+        message5: "<p>Play withthe configuration options to change the keywords, number of followers etc.</p>",
+        message6: "<p>Depending on your search configurations it can take a while before real time tweets show.</p>"
+        // message7: "<p>Play withthe configuration options to change the keywords, number of followers etc.</p>"
     }
 
 };
