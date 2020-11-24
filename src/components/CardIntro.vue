fbf1ddb4649033341afdc60b49a3eddd80ce0a0c
Kor Dwarshuis <kor@dwarshuis.com>
2020-11-24 13:58:50 +0100

    Border radius on bottom

--- src/components/CardIntro.vue
+++ src/components/CardIntro.vue
@@ -237,6 +237,8 @@ export default {
     bottom: 0;
     left: 0;
     background: linear-gradient(to right, #5C34A7, #2376D6);
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
 }
 
 // hr {
