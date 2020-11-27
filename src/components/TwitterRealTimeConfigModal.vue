a59f9bb4893339709b1af6aa665fb3bc70d9eb91
Kor Dwarshuis <kor@dwarshuis.com>
2020-11-27 10:21:28 +0100

    Default followers set to 750

--- src/components/TwitterRealTimeConfigModal.vue
+++ src/components/TwitterRealTimeConfigModal.vue
@@ -22,9 +22,9 @@
                             <h2 class="mt-3">Followers</h2>
                             <label class="" for="followers" id="labelFollowers">Poster has </label>
                             <select name="followers" id="followers">
-                                <option value="0" selected>0</option>
+                                <option value="0">0</option>
                                 <option value="500">500</option>
-                                <option value="750">750</option>
+                                <option value="750" selected>750</option>
                                 <option value="1000">1000</option>
                                 <option value="2000">2000</option>
                                 <option value="5000">5000</option>
