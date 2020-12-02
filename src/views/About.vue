bd0b29e01054ec9e1cf8fed821c4b0b264b4c953
Kor Dwarshuis <kor@dwarshuis.com>
2020-12-02 14:49:37 +0100

    Pulling content from external html file

---
+++ src/views/About.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="about pb-5">
+    <div class="container " v-html="content"></div>
+</div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    data: function () {
+        return {
+            content: ""
+        }
+    },
+    mounted() {
+        this.getContent();
+    },
+    methods: {
+        getContent() {
+        console.log('process.env.VUE_APP_ABOUT_PAGE_SOURCE: ', process.env.VUE_APP_ABOUT_PAGE_SOURCE);
+            return axios.get(process.env.VUE_APP_ABOUT_PAGE_SOURCE)
+                .then(response => {
+                    this.content = response.data;
+                }).catch(function (error) {
+                    console.log("We have a problem: " + error);
+                });
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
