170d72197546f95bd2530f1a356d8356c1194df7
Kor Dwarshuis <kor@dwarshuis.com>
2020-12-03 09:33:59 +0100

    More language

--- src/assets/js/language1.js
+++ src/assets/js/language1.js
@@ -10,8 +10,10 @@ export var language = {
         message3: "<h2>Step forward, spread bitcoin expertise</h2>",
         message4: "<h2>Get recognition, spread bitcoin expertise</h2>",
         message5: "<p>Play withthe configuration options to change the keywords, number of followers etc.</p>",
-        message6: "<p>Depending on your search configurations it can take a while before real time tweets show.</p>"
+        message6: "<p>Depending on your search configurations it can take a while before real time tweets show.</p>",
         // message7: "<p>Play withthe configuration options to change the keywords, number of followers etc.</p>"
+        anyOfTheseStrings: "bitcoin will never, bitcoin can never, bitcoin just is not, bitcoin is one big, criminals, slow, laundering, energy, complicated, unfair, quantum, tax evaders, unsustainable, intrinsic value, shut down, scammers, roulette, only 21, not safe, black market, terrorists, tulip, greater fool, not scalable, anarchists, distribution unfair, hacked, anonymous, unsustainable, useless, ponzi, no backing, will die, forbidden, shut down, scammers, not gdpr, price down, terrorists, privacy breach, volatile, useless, deflation, chinese",
+        noneOfTheseStrings: "I've hacked your computer,play online casino,Airdrop,All you have to do is RT,I will give one random follower,I will give someone"
     },
     expert1: "Eric Voskuil",
     expert1Description: "",
