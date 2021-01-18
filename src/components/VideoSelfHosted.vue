6cde68d1ba854db80df3d0a302f5a0723476a2a2
Kor Dwarshuis <kor@dwarshuis.com>
2021-01-18 10:36:15 +0100

    New file

---
+++ src/components/VideoSelfHosted.vue
@@ -0,0 +1,31 @@
+<template>
+<!-- negate doesnt seem to work -->
+<div v-if="(this.$store.state.currentCard['Self Hosted Video'])" class="content-item p-3">
+    <!-- <h3 class="video">Video</h3> -->
+
+    <VideoSelfHostedBare />
+
+    <p class="video-description" v-linkified:options="$store.state.linkifyOptions" v-for="item in this.$store.state.currentCard['Self Hosted Video Description']" v-bind:key="item">{{ item }}</p>
+
+</div>
+</template>
+
+<script>
+import store from "../store/store";
+import VideoSelfHostedBare from "@/components/VideoSelfHostedBare.vue";
+export default {
+    name: "VideoSelfHosted",
+    components: {
+        VideoSelfHostedBare
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style lang="scss" scoped>
+.video-description {
+    font-size: 0.9em;
+    margin-left: 0;
+}
+</style>
---
+++ src/components/VideoSelfHostedBare.vue
@@ -0,0 +1,46 @@
+<template>
+<!-- negate doesnt seem to work -->
+<div v-if="(this.$store.state.currentCard['Self Hosted Video'])" class="">
+    <div class="videoWrapper">
+        <video :src="path" controls></video>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "VideoSelfHostedBare",
+    data() {
+        return {
+            path: process.env.VUE_APP_MEDIA_LOCATION + this.$store.state.currentCard['Self Hosted Video']
+        }
+    },
+    mounted: function () {
+    },
+    methods: {
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style lang="scss" scoped>
+/* https://css-tricks.com/NetMag/FluidWidthVideo/Article-FluidWidthVideo.php */
+.videoWrapper {
+    position: relative;
+    padding-bottom: 56.25%;
+    /* 16:9 */
+    padding-top: 25px;
+    height: 0;
+    margin-bottom: 3em;
+}
+
+.videoWrapper video {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border: none;
+}
+</style>
