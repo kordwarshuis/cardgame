7990904ebe0cc58bed659c1af6eaf6108bf88c94
Kor Dwarshuis <kor@dwarshuis.com>
2021-01-24 13:28:00 +0100

    New file

---
+++ src/components/ImageSelfHosted.vue
@@ -0,0 +1,30 @@
+<template>
+<!-- negate doesnt seem to work -->
+<div v-if="(this.$store.state.currentCard['Self Hosted Image'])" class="content-item p-3">
+    <img :src="path + this.$store.state.currentCard['Self Hosted Image']" :alt="this.$store.state.currentCard['Self Hosted Image Alt']">
+    <p class="image-description" v-linkified:options="$store.state.linkifyOptions">{{this.$store.state.currentCard['Self Hosted Image Description']}}</p>
+</div>
+</template>
+
+<script>
+import store from "../store/store";
+import MediaSelfHostedBare from "@/components/MediaSelfHostedBare.vue";
+export default {
+    name: "ImageSelfHosted",
+    data() {
+        return {
+            path: process.env.VUE_APP_MEDIA_LOCATION
+        }
+    },
+    components: {
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style lang="scss" scoped>
+img {
+    width: 100%;
+}
+</style>
