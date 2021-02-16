cec62f1a886e99dc010a91ce1fdda5540963087e
Kor Dwarshuis <kor@dwarshuis.com>
2021-02-16 13:38:41 +0100

    Added top margin to images

--- src/components/ImagesSelfHosted.vue
+++ src/components/ImagesSelfHosted.vue
@@ -4,15 +4,15 @@
         <img class="mb-4" :src="path + this.$store.state.currentCard['Self Hosted Image 1']" :alt="this.$store.state.currentCard['Self Hosted Image Alt 1']">
         <p class="image-description" v-linkified:options="$store.state.linkifyOptions">{{this.$store.state.currentCard['Self Hosted Image Description 1']}}</p>
     </div>
-    <div v-if="(this.$store.state.currentCard['Self Hosted Image 2'])" class="content-item p-3">
+    <div v-if="(this.$store.state.currentCard['Self Hosted Image 2'])" class="content-item p-3 mt-3">
         <img class="mb-4" :src="path + this.$store.state.currentCard['Self Hosted Image 2']" :alt="this.$store.state.currentCard['Self Hosted Image Alt 2']">
         <p class="image-description" v-linkified:options="$store.state.linkifyOptions">{{this.$store.state.currentCard['Self Hosted Image Description 2']}}</p>
     </div>
-    <div v-if="(this.$store.state.currentCard['Self Hosted Image 3'])" class="content-item p-3">
+    <div v-if="(this.$store.state.currentCard['Self Hosted Image 3'])" class="content-item p-3 mt-3">
         <img class="mb-4" :src="path + this.$store.state.currentCard['Self Hosted Image 3']" :alt="this.$store.state.currentCard['Self Hosted Image Alt 3']">
         <p class="image-description" v-linkified:options="$store.state.linkifyOptions">{{this.$store.state.currentCard['Self Hosted Image Description 3']}}</p>
     </div>
-    <div v-if="(this.$store.state.currentCard['Self Hosted Image 4'])" class="content-item p-3">
+    <div v-if="(this.$store.state.currentCard['Self Hosted Image 4'])" class="content-item p-3 mt-3">
         <img class="mb-4" :src="path + this.$store.state.currentCard['Self Hosted Image 4']" :alt="this.$store.state.currentCard['Self Hosted Image Alt 4']">
         <p class="image-description" v-linkified:options="$store.state.linkifyOptions">{{this.$store.state.currentCard['Self Hosted Image Description 4']}}</p>
     </div>
