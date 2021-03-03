7066f63d63ddbba2aab25a3425a31625c6e76f4f
kor <kor.dwarshuis@gmail.com>
2021-03-03 17:08:28 +0100

    Removed path variable

--- src/components/ImageSelfHosted.vue
+++ src/components/ImageSelfHosted.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
     <div v-if="(this.$store.state.currentCard['Self Hosted Image 1'])" class="content-item p-3">
-        <img class="mb-4" :src="path + this.$store.state.currentCard['Self Hosted Image 1']" :alt="this.$store.state.currentCard['Self Hosted Image Alt 1']">
+        <img class="mb-4" :src="this.$store.state.currentCard['Self Hosted Image 1']" :alt="this.$store.state.currentCard['Self Hosted Image Alt 1']">
         <p class="image-description" v-linkified:options="$store.state.linkifyOptions">{{this.$store.state.currentCard['Self Hosted Image Description 1']}}</p>
     </div>
 </div>
@@ -9,14 +9,7 @@
 
 <script>
 export default {
-    name: "ImageSelfHosted",
-    data() {
-        return {
-            path: process.env.VUE_APP_MEDIA_LOCATION
-        }
-    },
-    components: {
-    }
+    name: "ImageSelfHosted"
 };
 </script>
 
--- src/components/ImagesSelfHosted.vue
+++ src/components/ImagesSelfHosted.vue
@@ -1,19 +1,19 @@
 <template>
 <div>
     <div v-if="(this.$store.state.currentCard['Self Hosted Image 1'])" class="content-item p-3">
-        <img class="mb-4" :src="path + this.$store.state.currentCard['Self Hosted Image 1']" :alt="this.$store.state.currentCard['Self Hosted Image Alt 1']">
+        <img class="mb-4" :src="this.$store.state.currentCard['Self Hosted Image 1']" :alt="this.$store.state.currentCard['Self Hosted Image Alt 1']">
         <p class="image-description" v-linkified:options="$store.state.linkifyOptions">{{this.$store.state.currentCard['Self Hosted Image Description 1']}}</p>
     </div>
     <div v-if="(this.$store.state.currentCard['Self Hosted Image 2'])" class="content-item p-3 mt-3">
-        <img class="mb-4" :src="path + this.$store.state.currentCard['Self Hosted Image 2']" :alt="this.$store.state.currentCard['Self Hosted Image Alt 2']">
+        <img class="mb-4" :src="this.$store.state.currentCard['Self Hosted Image 2']" :alt="this.$store.state.currentCard['Self Hosted Image Alt 2']">
         <p class="image-description" v-linkified:options="$store.state.linkifyOptions">{{this.$store.state.currentCard['Self Hosted Image Description 2']}}</p>
     </div>
     <div v-if="(this.$store.state.currentCard['Self Hosted Image 3'])" class="content-item p-3 mt-3">
-        <img class="mb-4" :src="path + this.$store.state.currentCard['Self Hosted Image 3']" :alt="this.$store.state.currentCard['Self Hosted Image Alt 3']">
+        <img class="mb-4" :src="this.$store.state.currentCard['Self Hosted Image 3']" :alt="this.$store.state.currentCard['Self Hosted Image Alt 3']">
         <p class="image-description" v-linkified:options="$store.state.linkifyOptions">{{this.$store.state.currentCard['Self Hosted Image Description 3']}}</p>
     </div>
     <div v-if="(this.$store.state.currentCard['Self Hosted Image 4'])" class="content-item p-3 mt-3">
-        <img class="mb-4" :src="path + this.$store.state.currentCard['Self Hosted Image 4']" :alt="this.$store.state.currentCard['Self Hosted Image Alt 4']">
+        <img class="mb-4" :src="this.$store.state.currentCard['Self Hosted Image 4']" :alt="this.$store.state.currentCard['Self Hosted Image Alt 4']">
         <p class="image-description" v-linkified:options="$store.state.linkifyOptions">{{this.$store.state.currentCard['Self Hosted Image Description 4']}}</p>
     </div>
 </div>
@@ -21,14 +21,7 @@
 
 <script>
 export default {
-    name: "ImagesSelfHosted",
-    data() {
-        return {
-            path: process.env.VUE_APP_MEDIA_LOCATION
-        }
-    },
-    components: {
-    }
+    name: "ImagesSelfHosted"
 };
 </script>
 
