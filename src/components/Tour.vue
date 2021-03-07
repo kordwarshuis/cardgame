55752e41a5da0f4132f9521083bef331819ce252
kor <kor.dwarshuis@gmail.com>
2021-03-07 21:47:04 +0100

    Added external js file via script tag

---
+++ src/components/Tour.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-tour name="myTour" :steps="steps"></v-tour>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'my-tour',
+    data () {
+      return {
+        steps: tour // reading from script imported via index.html TODO: make more robust
+      }
+    },
+    mounted: function () {
+      this.$tours['myTour'].start()
+    }
+  }
+</script>
