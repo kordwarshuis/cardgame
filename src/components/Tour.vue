d91df635f05530ba39051565f71c62373826193f
kor <kor.dwarshuis@gmail.com>
2021-03-08 15:29:04 +0100

    Many changes

--- src/components/Tour.vue
+++ src/components/Tour.vue
@@ -1,19 +1,44 @@
 <template>
-  <div>
-    <v-tour name="myTour" :steps="steps"></v-tour>
-  </div>
+<div>
+    <div class="alert alert-info m-5 startTour" role="alert">
+        <button @click="startTour" type="button" class="btn btn-info tour-start">{{tourLanguage.startTour}}</button>
+        <button @click="turnOffTour" type="button" class="btn btn-info">{{tourLanguage.dontShowAgain}}</button>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    <v-tour name="myTour" :steps="tour.steps" :options="tour.options"></v-tour>
+</div>
 </template>
 
 <script>
-  export default {
+export default {
     name: 'my-tour',
-    data () {
-      return {
-        steps: tour // reading from script imported via index.html TODO: make more robust
-      }
+    data() {
+        return {
+            tour: tour,
+            tourLanguage: language.tour // reading from script imported via index.html TODO: make more robust
+        }
     },
     mounted: function () {
-      this.$tours['myTour'].start()
+        this.checkTourSetting();
+    },
+    methods: {
+        checkTourSetting() {
+            if (localStorage.getItem('showTour') === 'false') {
+                document.addEventListener("DOMContentLoaded", function (event) {
+                    $('.startTour').alert('close');
+                });
+            }
+        },
+        startTour() {
+            this.$tours['myTour'].start();
+            $('.startTour').alert('close');
+        },
+        turnOffTour() {
+            localStorage.setItem('showTour', 'false');
+            $('.startTour').alert('close');
+        }
     }
-  }
+}
 </script>
