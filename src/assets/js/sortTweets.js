f608ec4d9aff0e68c0e0b2a3e1414e9293581789
kor <kor.dwarshuis@gmail.com>
2021-03-19 20:21:21 +0100

    Sorting dom nodes

--- src/assets/js/sortTweets.js
+++ src/assets/js/sortTweets.js
@@ -1,65 +1,49 @@
+import {
+    tweetStreamAttentionSeeker
+} from '@/assets/js/tweetStreamAttentionSeeker';
+
 export function sort(key) {
+    // https://stackoverflow.com/a/282711 modified
+    var container = document.querySelector(".tweets-realtime .tweets");
+    var items = document.querySelectorAll(".tweets-realtime .tweet");
+    // tweet at the top, anchor needed for scrolling to top
+    var topTweet = document.querySelector("#topTweet");
 
-    // console.log("sorting started");
-    // Declaring Variables 
-    var geek_list, i, run, li, stop;
+    var itemsArr = [];
+    var aa = 0;
+    var bb = 0;
 
-    // Taking content of list as input 
-
-    // setTimeout(function(){
-    //     geek_list = document.querySelector(".tweets-realtime");
-    // console.log('geek_list: ', geek_list);
-    // }, 1000);
-    geek_list = document.querySelector(".tweets-realtime .tweets");
-    // console.log('geek_list: ', geek_list);
-
-    run = true;
-
-    while (run) {
-        run = false;
-        li = geek_list.querySelectorAll('.tweet');
-        // console.log('li: ', li);
-
-        // Loop traversing through all the list items 
-        for (i = 0; i < (li.length - 1); i++) {
-            stop = false;
-            console.log('key: ', li[i].querySelector(key));
-            console.log('XXXXXX: ', li[i].querySelector(key));
-            // if (parseInt(li[i].querySelector(key).dataset[key].trim(), 10) <
-            //     parseInt(li[i + 1].querySelector(key).dataset[key].trim(), 10)) {
-            //     stop = true;
-            //     break;
-            // }
+    for (var i in items) {
+        if (items.hasOwnProperty(i)) {
+            itemsArr.push(items[i]);
         }
-
-        /* If the current item is smaller than  
-           the next item then adding it after  
-           it using insertBefore() method */
-        if (stop) {
-            document.querySelector(".tweets-realtime .tweets").insertBefore(
-                li[i + 1], li[i]);
-
-            run = true;
-            
-        }
-
-
     }
 
+    itemsArr.sort(function (a, b) {
+        aa = parseInt(a.querySelector('.' + key).dataset[key], 10);
+        bb = parseInt(b.querySelector('.' + key).dataset[key], 10);
 
-    // document.querySelector(".tweets-realtime .tweets").scrollTop = 0;
+        return aa === bb ?
+            0 :
+            (aa < bb ? 1 : -1);
+    });
 
+    for (i = 0; i < itemsArr.length; ++i) {
+        container.appendChild(itemsArr[i]);
+    }
 
-    var topTweet = document.querySelector("#topTweet");
-        
+    // remove anchor if exists (if sort was done before)
     if (topTweet) {
-        // topTweet.remove();
         topTweet.removeAttribute('id');
     }
-    
+    // add anchor
     document.querySelector(".tweets-realtime .tweets .tweet").setAttribute('id', 'topTweet');
 
-    // document.querySelector("#topTweet").scrollTo(0, 0);
-    document.querySelector("#topTweet").scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
-
+    // scroll
+    document.querySelector("#topTweet").scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+        inline: "nearest"
+    });
+    tweetStreamAttentionSeeker();
 }
