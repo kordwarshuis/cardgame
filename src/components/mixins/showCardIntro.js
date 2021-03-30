102def90c460d1555802a73a8f599262e44d53e4
kor <kor.dwarshuis@gmail.com>
2021-03-30 08:12:28 +0200

    Removed console.log and unused code

--- src/components/mixins/showCardIntro.js
+++ src/components/mixins/showCardIntro.js
@@ -2,33 +2,6 @@ import store from "../../store/store";
 export default {
     methods: {
         showCardIntro() {
-            console.log("radio");
-
-            // console.log('event: ', event);
-            console.log("radio");
-
-
-            // event.target.closest(".card").classList.add("visited");
-            // console.log('event.target.closest(".card"): ', event.target.closest(".card"));
-
-            // // https://stackoverflow.com/a/7680123
-            // // localStorage can only be string
-            // function appendToLocalStorage(name, data) {
-            //     var old = localStorage.getItem(name);
-
-            //     // create if not existing
-            //     if (old === null) old = "";
-
-            //     // only add if it is not already in the string
-            //     if (old.indexOf(data) === -1) localStorage.setItem(name, old + data + " ");
-            //     console.log(localStorage.getItem("visited"));
-            // }
-
-            // // add current card url to visited in localStorage
-            // appendToLocalStorage("visited", this.$store.state.currentCard["Unique URL"]);
-
-            // this.addVisitedToCards();
-
             // deal with CSS to open and close
             this.$store.commit("changeCssClassCardIntroState", "open");
             this.$store.commit("changeCssClassCardOverviewState", "overlay-fullscreen-open");
