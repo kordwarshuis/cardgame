c117f3dcd9918f540b6fe57a6fe6c007a3322f98
kor <kor.dwarshuis@gmail.com>
2021-04-30 15:24:58 +0200

    Lazyloading now works

--- src/assets/js/lazyloadImages.js
+++ src/assets/js/lazyloadImages.js
@@ -1,58 +1,65 @@
 export function lazyloadImages() {
-    document.addEventListener("DOMContentLoaded", function () {
-        var lazyloadImages;
+    // https://www.smashingmagazine.com/2018/01/deferring-lazy-loading-intersection-observer-api/ -> https://codepen.io/mishunov/pen/qpmWYP
+    var lazyloadImages;
 
-        if ("IntersectionObserver" in window) {
-            lazyloadImages = document.querySelectorAll(".lazy");
-            var imageObserver = new IntersectionObserver(function (entries, observer) {
-                console.log(observer);
-                entries.forEach(function (entry) {
-                    // console.log('entry.isIntersecting: ', entry.isIntersecting);
+    if ("IntersectionObserver" in window) {
+        const images = document.querySelectorAll('[data-src]');
+        const config = {
+            rootMargin: '0px 0px 50px 0px',
+            threshold: 0
+        };
+        let loaded = 0;
 
-                    if (entry.isIntersecting) {
-                        // console.log('entry.isIntersecting: ', entry.isIntersecting);
-                        var image = entry.target;
-                        image.src = image.dataset.src;
-                        image.classList.remove("lazy");
-                        imageObserver.unobserve(image);
-                    }
-                });
-            }, {
-                root: document.querySelector(".tweets"),
-                rootMargin: "0px 0px 0px 0px"
-            });
-
-            lazyloadImages.forEach(function (image) {
-                imageObserver.observe(image);
-            });
-        } else {
-            var lazyloadThrottleTimeout;
-            lazyloadImages = document.querySelectorAll(".lazy");
-
-            function lazyload() {
-                if (lazyloadThrottleTimeout) {
-                    clearTimeout(lazyloadThrottleTimeout);
+        let observer = new IntersectionObserver(function (entries, self) {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    // console.log(`Image ${entry.target.src} is in the viewport!`);
+                    preloadImage(entry.target);
+                    // Stop watching and load the image
+                    self.unobserve(entry.target);
                 }
+            });
+        }, config);
 
-                lazyloadThrottleTimeout = setTimeout(function () {
-                    var scrollTop = window.pageYOffset;
-                    lazyloadImages.forEach(function (img) {
-                        if (img.offsetTop < (window.innerHeight + scrollTop)) {
-                            img.src = img.dataset.src;
-                            img.classList.remove('lazy');
-                        }
-                    });
-                    if (lazyloadImages.length == 0) {
-                        document.removeEventListener("scroll", lazyload);
-                        window.removeEventListener("resize", lazyload);
-                        window.removeEventListener("orientationChange", lazyload);
-                    }
-                }, 20);
+        images.forEach(image => {
+            observer.observe(image);
+        });
+
+        function preloadImage(img) {
+            const src = img.getAttribute('data-src');
+            if (!src) {
+                return;
+            }
+            img.src = src;
+        }
+    } else {
+        // https://css-tricks.com/the-complete-guide-to-lazy-loading-images/
+        var lazyloadThrottleTimeout;
+        lazyloadImages = document.querySelectorAll(".lazy");
+
+        function lazyload() {
+            if (lazyloadThrottleTimeout) {
+                clearTimeout(lazyloadThrottleTimeout);
             }
 
-            document.addEventListener("scroll", lazyload);
-            window.addEventListener("resize", lazyload);
-            window.addEventListener("orientationChange", lazyload);
+            lazyloadThrottleTimeout = setTimeout(function () {
+                var scrollTop = window.pageYOffset;
+                lazyloadImages.forEach(function (img) {
+                    if (img.offsetTop < (window.innerHeight + scrollTop)) {
+                        img.src = img.dataset.src;
+                        img.classList.remove('lazy');
+                    }
+                });
+                if (lazyloadImages.length == 0) {
+                    document.removeEventListener("scroll", lazyload);
+                    window.removeEventListener("resize", lazyload);
+                    window.removeEventListener("orientationChange", lazyload);
+                }
+            }, 20);
         }
-    });
+
+        document.addEventListener("scroll", lazyload);
+        window.addEventListener("resize", lazyload);
+        window.addEventListener("orientationChange", lazyload);
+    }
 }
