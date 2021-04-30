07c6c5354fec150f28e7893d98c0b38018cc8725
kor <kor.dwarshuis@gmail.com>
2021-04-30 10:42:11 +0200

    Disabled focus / visibilitychange

--- src/assets/js/getJSON.js
+++ src/assets/js/getJSON.js
@@ -10,6 +10,47 @@ import {
     tweetsHistory
 } from "./tweetsHistory.js";
 
+// https://stackoverflow.com/a/1060034
+(function() {
+    var hidden = "hidden";
+  
+    // Standards:
+    if (hidden in document)
+      document.addEventListener("visibilitychange", onchange);
+    else if ((hidden = "mozHidden") in document)
+      document.addEventListener("mozvisibilitychange", onchange);
+    else if ((hidden = "webkitHidden") in document)
+      document.addEventListener("webkitvisibilitychange", onchange);
+    else if ((hidden = "msHidden") in document)
+      document.addEventListener("msvisibilitychange", onchange);
+    // IE 9 and lower:
+    else if ("onfocusin" in document)
+      document.onfocusin = document.onfocusout = onchange;
+    // All others:
+    else
+      window.onpageshow = window.onpagehide
+      = window.onfocus = window.onblur = onchange;
+  
+    function onchange (evt) {
+      var v = "visible", h = "hidden",
+          evtMap = {
+            focus:v, focusin:v, pageshow:v, blur:h, focusout:h, pagehide:h
+          };
+  
+      evt = evt || window.event;
+      if (evt.type in evtMap)
+        document.body.className = evtMap[evt.type];
+      else
+        document.body.className = this[hidden] ? "hidden" : "visible";
+    }
+  
+    // set the initial state (but only if browser supports the Page Visibility API)
+    if( document[hidden] !== undefined )
+      onchange({type: document[hidden] ? "blur" : "focus"});
+  })();
+
+
+
 // load tweets history on load
 document.addEventListener("DOMContentLoaded", tweetsHistory, false);
 
@@ -18,15 +59,17 @@ function handleVisibilityChange() {
     if (document.hidden) {
         //   code
     } else {
+        console.log("visibilitychange");
         tweetsHistory();
     }
 }
 
-document.addEventListener("visibilitychange", handleVisibilityChange, false);
+// document.addEventListener("visibilitychange", handleVisibilityChange, false);
 
-window.onfocus = function () {
-    tweetsHistory();
-};
+// window.onfocus = function () {
+//     console.log("focus");
+//     tweetsHistory();
+// };
 
 
 export var getJSON = (function () {
